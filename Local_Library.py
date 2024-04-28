d20ae29074ffc3c25c63b81467e594a6993d9e1b
s2-bolisetti <siva2.bolisetti@live.uwe.ac.uk>
2024-04-29 00:50:19 +0100

    .

--- Local_Library.py
+++ Local_Library.py
@@ -7,36 +7,28 @@ from statsmodels.tsa.arima.model import ARIMA
 def kalman_filter(data):
     # Source Code is stored locally as the support for python inbuilt function is no longer supported after one of the recent python updates.
     # Welch, G. and Bishop, G., 1995. An introduction to the Kalman filter., pages 11-15
-    # https://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
-    
+    # https://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf    
     # data: input numpy.ndarray data
     
     data_shape = data.shape # Shape of the data.
-    process_variance = 2e-4 # Estimated process variance, Q.
-    
+    process_variance = 2e-4 # Estimated process variance, Q.    
     # Instantiating default vectors for filter parameters
     x_hat = np.zeros(data_shape)      # a posteriori state estimate of x
     P = np.zeros(data_shape)         # a posteriori estimate error covariance
     x_hat_bar = np.zeros(data_shape) # a priori state estimate of x
     P_bar = np.zeros(data_shape)    # a priori estimate error covariance
     K = p.zeros(data_shape)         # gain or blending factor that minimizes the a posteriori error covariance
-
-    R = 0.1**2 # estimate of measurement error covariance
-
-    
+    R = 0.1**2 # estimate of measurement error covariance    
     x_hat[0] = 0.0 # Initial state value for posteriori state estimate of x
     P[0] = 1.0 # Initial state value for posteriori estimate error covariance
-
     for data_shape_iter in range(1,data_shape[0]):
         # time update filter parameter equations
         x_hat_bar[data_shape_iter] = x_hat[data_shape_iter-1]
         P_bar[data_shape_iter] = P[data_shape_iter-1]+process_variance
-
         # measurement update filter parameter equations
         K[data_shape_iter] = P_bar[data_shape_iter]/( P_bar[data_shape_iter]+R )
         x_hat[data_shape_iter] = x_hat_bar[data_shape_iter]+K[data_shape_iter]*(data[data_shape_iter]-x_hat_bar[data_shape_iter])
         P[data_shape_iter] = (1-K[data_shape_iter])*P_bar[data_shape_iter]        
-    
     return x_hat
    
    
